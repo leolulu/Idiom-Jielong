373f9033c053cc6afa307d9ab15f27cd470beb77
surface@pro.3 <348699103@qq.com>
2020-12-27 10:46:28 +0800

    准备开始做模糊拼音

---
+++ main.py
@@ -0,0 +1,8 @@
+from scripts.jielong import JieLong
+
+
+
+if __name__ == "__main__":
+    j = JieLong()
+    result = j.query_by_py1('li')
+    print(result)
--- scripts/jielong.py
+++ scripts/jielong.py
@@ -1,21 +1,21 @@
 import os
 import csv
-import time
+# from utils.decorator_utils import get_exec_time
 
 
 class JieLong:
     '''
     data stucture:
-    idiom, freq, py, pinyin1, tone1, pinyin2, tone2, pinyin3, tone3, pinyin4, tone4
+    idiom, freq, py, py1, tone1, py2, tone2, py3, tone3, py4, tone4
     '''
+
     def __init__(self):
-        self.asset_path = self.get_asset_path()
-
-
-
+        self.get_asset_path()
+        self.get_idiom_data()
+        self.switch_only_top1_freq = True
 
     def get_asset_path(self):
-        return os.path.join(
+        self.asset_path = os.path.join(
             os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
             'assets',
             '成语结果.csv'
@@ -25,11 +25,19 @@ class JieLong:
         with open(self.asset_path, 'r', encoding='utf-8') as f:
             csv_data = csv.DictReader(f)
             csv_data = [i for i in csv_data]
-        self.csv_data = csv_data
+        self.idiom_data = csv_data
 
-    def query_by(self,pingyin):
-        pass
+    # @get_exec_time
+    def query_by_py1(self, pingyin):
+        result = [i['idiom'] for i in self.idiom_data if i['py1'] == pingyin]
+        return result
+
+    def get_freq_list(self):
+        freq_list = list(set([i['freq'] for i in self.idiom_data]))
+        freq_list.sort(reverse=True)
+        print(freq_list)
+
 
 if __name__ == "__main__":
-    data = get_idiom_data(get_assets_path())
-    print(data)
+    f = JieLong()
+    f.get_freq_list()
